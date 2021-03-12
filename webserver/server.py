a3f72b56f255a2c63bf5f86775384a97923f352b
Sedona Thomas <sntsregister@gmail.com>
2021-03-11 17:53:47 -0800

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -19,7 +19,6 @@ app = Flask(__name__, template_folder=tmpl_dir)
 
 # postgresql://USER:PASSWORD@34.73.36.248/project1
 DATABASEURI = "postgresql://jjk2235:512791@34.73.36.248/project1"
-
 engine = create_engine(DATABASEURI)
 
 @app.before_request
@@ -28,7 +27,6 @@ def before_request():
   This function is run at the beginning of every web request 
   (every time you enter an address in the web browser).
   We use it to setup a database connection that can be used throughout the request.
-
   The variable g is globally accessible.
   """
   try:
@@ -50,29 +48,14 @@ def teardown_request(exception):
     pass
 
 
-#
-# @app.route is a decorator around index() that means:
-#   run index() whenever the user tries to access the "/" path using a GET request
-#
-# If you wanted the user to go to, for example, localhost:8111/foobar/ with POST or GET then you could use:
-#
-#       @app.route("/foobar/", methods=["POST", "GET"])
-#
-# PROTIP: (the trailing / in the path is important)
-# 
-# see for routing: https://flask.palletsprojects.com/en/1.1.x/quickstart/#routing
-# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
-#
 # localhost:8111/
 @app.route('/')
 def index():
   """
   request is a special object that Flask provides to access web request information:
-
-  request.method:   "GET" or "POST"
-  request.form:     if the browser submitted a form, this contains the data in the form
-  request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
-
+      request.method:   "GET" or "POST"
+      request.form:     if the browser submitted a form, this contains the data in the form
+      request.args:     dictionary of URL arguments, e.g., {a:1, b:2} for http://localhost?a=1&b=2
   See its API: https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data
   """
   
@@ -95,22 +78,46 @@ def index():
 # localhost:8111/packages
 @app.route('/packages')
 def packages():
-  return render_template("packages.html")
+  cursor = g.conn.execute("SELECT * FROM packages")
+  data = []
+  for result in cursor:
+    data.append(result)
+  cursor.close()
+  context = dict(data = data)
+  return render_template("packages.html", **context)
 
 # localhost:8111/modules
 @app.route('/modules')
 def modules():
-  return render_template("modules.html")
+  cursor = g.conn.execute("SELECT * FROM modules")
+  data = []
+  for result in cursor:
+    data.append(result)
+  cursor.close()
+  context = dict(data = data)
+  return render_template("modules.html", **context)
 
 # localhost:8111/methods
 @app.route('/methods')
 def methods():
-  return render_template("methods.html")
+  cursor = g.conn.execute("SELECT * FROM methods")
+  data = []
+  for result in cursor:
+    data.append(result)
+  cursor.close()
+  context = dict(data = data)
+  return render_template("methods.html", **context)
 
 # localhost:8111/constants
 @app.route('/constants')
 def constants():
-  return render_template("constants.html")
+  cursor = g.conn.execute("SELECT * FROM constants")
+  data = []
+  for result in cursor:
+    data.append(result)
+  cursor.close()
+  context = dict(data = data)
+  return render_template("constants.html", **context)
 
 
 '''
@@ -157,6 +164,10 @@ if __name__ == "__main__":
 '''
     Miscellaneous
 '''
+
+# @app.route("/foobar/", methods=["POST", "GET"])
+# see for routing: https://flask.palletsprojects.com/en/1.1.x/quickstart/#routing
+# see for decorators: http://simeonfranklin.com/blog/2012/jul/1/python-decorators-in-12-steps/
 
 '''
 # localhost:8111/another_page
