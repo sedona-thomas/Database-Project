c78ca7188b2fabdc4dc44b1a9085622eeb86c8c4
Sedona Thomas <sntsregister@gmail.com>
2021-03-21 17:45:13 -0700

    Update server.py

--- webserver/server.py
+++ webserver/server.py
@@ -229,14 +229,16 @@ def keyword_results():
 # looks up by related keywords and selected type of thing
 @app.route('/related_keyword_search',methods=['POST'])
 def related_keyword_search():
-  #command = text("SELECT {1}.* FROM {1}, {1}_keywords WHERE ('{0}' = {1}_keywords.keyword) AND ({1}_keywords.name = {1}.name)".format(request.form['keyword'], request.form['type_name']))
+  # SELECT similar_keywords.keyword1 FROM similar_keywords WHERE similar_keywords.keyword1 = 'e' OR similar_keywords.keyword2 = 'e' UNION SELECT similar_keywords.keyword2 FROM similar_keywords WHERE similar_keywords.keyword1 = 'e' OR similar_keywords.keyword2 = 'e';
+  # SELECT similar_keywords.keyword1 FROM similar_keywords WHERE similar_keywords.keyword1 = {0} OR similar_keywords.keyword2 = {0} UNION SELECT similar_keywords.keyword2 FROM similar_keywords WHERE similar_keywords.keyword1 = {0} OR similar_keywords.keyword2 = {0}
+  command = text("SELECT {1}.* FROM {1}, {1}_keywords WHERE ({1}_keywords.keyword in SELECT similar_keywords.keyword1 FROM similar_keywords WHERE similar_keywords.keyword1 = {0} OR similar_keywords.keyword2 = {0} UNION SELECT similar_keywords.keyword2 FROM similar_keywords WHERE similar_keywords.keyword1 = {0} OR similar_keywords.keyword2 = {0}) AND ({1}_keywords.name = {1}.name)".format(request.form['keyword'], request.form['type_name']))
   cursor = g.conn.execute(command)
   data = []
   for result in cursor:
     data.append(result)
   cursor.close()
   context = dict(data = data)
-  context["title"] = "Searched {} from {}".format(request.form['keyword'], request.form['type_name'])
+  context["title"] = "Searched related to {} from {}".format(request.form['keyword'], request.form['type_name'])
   return render_template("keyword_results.html", **context)
 
 # looks up by keyword and selected type of thing in favorites
